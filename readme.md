471c301a3be350e9ce573f864a47409c7e5e97bc
Thinkedou <r.enguehard@gmail.com>
2022-10-10 19:50:29 +0200

    init

---
+++ readme.md
@@ -0,0 +1,34 @@
+# Disco III ~ Tailwind ~ Vue3
+
+Intégrer une maquette statique dans vue3 et travailler les directives et les events 
+
+## Importer les assets
+Importer le layout et les assets (css, js, img) 
+## Créer une data reactive()
+Rendre l'album d'exemple dynamique
+
+## Rajouter un album de son choix
+Histoire de manipuler un peu le data-model
+
+## v-for
+Charger allRecords.js et afficher tous les albums, attention à **:key**
+## Les images 
+Switcher sur l'image default.jpg si un album n'a pas de cover 
+
+## Gestion des stocks 
+Permettre la gestion des stocks via les btns [+] & [-]
+
+## Filtres
+Brancher les filtres  
+
+## v-if ou v-show pour picto stock
+Afficher le bon picto en fonction du stock de l'album
+
+## mettre des albums en favoris 
+un click sur la card > fav, un autre > unfav 
+Lier une class si fav pour highlight *border-rose-600*
+? est-ce que les btns stock marchent toujours?
+
+## afficher le stock total avec computed & reduce()
+Afficher ça à côté du title dans la nav ou dans la zone filtre
+
---
+++ static/js/allRecords.js
@@ -0,0 +1,94 @@
+export const records = [
+    {
+        "id"       :   3,
+        "title"    :'LP1',
+        "artist"   :'FKA twigs',
+        "year"     : '2014',
+        "stock"    : 0,
+        "pitchforkPos"   : 28,
+        "comment"      : "FKA twigs is already so fully developed as an artist that her voice—wispy, breathy, disarmingly operatic—can sometimes be overshadowed by her astonishing performances. They function as demonstrations of her dancer’s command of her body, living art installations, and full-spectrum auditory experience—together, something like live 4DX",
+        "coverUrl" : 'https://media.pitchfork.com/photos/5d7a863e0db1d10008f20007/master/w_1600%2Cc_limit/FKA-twigs-Two-Weeks.jpeg'
+    },
+    {
+        "id"             : 1,
+        "title"          :'In Colour',
+        "artist"         :'Jamie xx',
+        "year"           :'2015',
+        "stock"          :2,
+        "pitchforkPos"   :25,
+        "comment"        : "Hearing In Colour feels like listening to the dissection of a heartbeat—skittering anxieties mirrored in the snares, the warm glow of love reflected in steel drums, camaraderie sampled via musical voices of the past.",
+        "coverUrl"       :'https://media.pitchfork.com/photos/5d7bf74a6eee70000829f8c6/master/w_1600%2Cc_limit/Jamie-xx-In-Colour.jpg'
+    },
+    {
+        "id"             : 2,
+        "title"          :'Double Cup',
+        "artist"         :'DJ Rashad',
+        "year"           :'2013',
+        "stock"          :0,
+        "comment"        : "DJ Rashad was a singular genius and one of footwork’s architects, who shared credit for this unselfishly with other members of his inimitable Teklife crew",
+        "pitchforkPos"   :20
+    },
+
+    {
+        "id"             : 7,
+        "title"          :'New Amerykah Part Two: Return of the Ankh',
+        "artist"         :'Erykah Badu',
+        "year"           :'2010',
+        "stock"          :4,
+        "pitchforkPos"   :42,
+        "comment"        : "Then, now, and forever, love makes people go a little crazy. Even Erykah Badu, goddess of unbothered R&B, is not immune. Throughout Return of the Ankh, her only proper album of this decade, she offers up conflicting desires with the unfiltered realness of a drunk dial.",
+        "coverUrl"       :'https://media.pitchfork.com/photos/5d7bcd3d53e80c0008f43b28/master/w_1600%2Cc_limit/Erykah-Badu-New-Amerykah-Part-2.jpg'
+    },
+    {
+        "id"             : 8,
+        "title"          :'Room 25',
+        "artist"         :'Noname',
+        "year"           :'2018',
+        "stock"          :3,
+        "pitchforkPos"   :160,
+        "comment"        : "Noname once quipped that her style is “lullaby rap,” in reference to her soft timbre and relaxed cadence",
+        "coverUrl"       :'https://media.pitchfork.com/photos/5d94b58ea540790008f9dbb0/master/w_1600,c_limit/Noname-Room25.jpg'
+    },
+    {
+        "id"             : 4,
+        "title"          :'good kid, m.A.A.d. city',
+        "artist"         :'Kendrick Lamar',
+        "year"           :'2012',
+        "stock"          :3,
+        "pitchforkPos"   :18,
+        "comment"        : "On his major-label debut, Kendrick Lamar rapped like he’d been holding a breath his whole life, and when he exhaled, everything came out: his troubled and reverent relationship to West Coast gangsta rap",
+        "coverUrl"       :'https://media.pitchfork.com/photos/5d8901df1f868e00096102c2/master/w_1600%2Cc_limit/Kendrick-Lamar-good-kid-maad-city.jpg'
+    },
+    {
+        "id"             : 5,
+        "title"          :'Art Angels',
+        "artist"         :'Grimes',
+        "year"           :'2015',
+        "stock"          :0,
+        "pitchforkPos"   :11,
+        "comment"        : "Grimes is a producer, a provocateur, a punk—and, yes, a pop star. By her own estimation, pop is “music that seeks to hit the pleasure center,” and Art Angels, replete with surging dance beats and saccharine girl-gang vocals, undeniably fits the bill. But on Claire Boucher’s fourth album, her pleasure comes cut with danger",
+        "coverUrl"       :'https://media.pitchfork.com/photos/5d8904bc21339a000839699c/master/w_1600%2Cc_limit/Grimes-Art-Angels.jpg'
+    },
+    {
+        "id"             : 6,
+        "title"          :'Beyoncé',
+        "artist"         :'Beyoncé',
+        "year"           :'2013',
+        "stock"          :0,
+        "pitchforkPos"   :3,
+        "comment"        : "“We be all night,” Beyoncé proclaimed on “Drunk in Love,” the most meme-ready track on her self-titled album. She was singing about riding her husband’s big body like a surfboardt, but she may as well have been referring to the way listeners gulped down Beyoncé, which dropped at midnight on a Thursday and seemed to envelop all of social media into the dawn hours while also revolutionizing how albums are conceived and released.",
+        "coverUrl"       :'https://media.pitchfork.com/photos/5d89069e5d366d000838ce6e/master/w_1600%2Cc_limit/Beyonce%2525CC%252581-Beyonce%2525CC%252581.jpg'
+    },
+    {
+        "id"             : 9,
+        "title"          :'Sunbather',
+        "artist"         :'Deafheaven',
+        "year"           :'2013',
+        "stock"          :7,
+        "pitchforkPos"   :123,
+        "comment"        : "Clarke screams like a man in fear for his own life as the song’s frenzied guitar tremolos",
+        "coverUrl"       :'https://media.pitchfork.com/photos/5d94b58ea540790008f9dbb0/master/w_1600,c_limit/Noname-Room25.jpg'
+    }
+
+
+]
